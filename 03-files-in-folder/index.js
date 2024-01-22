211c69f9629311634b5240511a5280aad217da45
Dina P <dinchik@bk.ru>
2024-01-22 20:14:38 +0400

    refactor: add meaningful variable for unused error argument in 03-files-in-folder task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -6,11 +6,11 @@ const stdout = process.stdout;
 const secretFolderPath = path.join(__dirname, 'secret-folder');
 const options = { withFileTypes: true };
 
-fs.readdir(secretFolderPath, options, (_, files) => {
+fs.readdir(secretFolderPath, options, (_error, files) => {
   files.forEach((file) => {
     if (file.isFile()) {
       const currentPath = path.join(secretFolderPath, file.name);
-      fs.stat(currentPath, (_, stats) => {
+      fs.stat(currentPath, (_error, stats) => {
         const [fileName, fileExtension] = file.name.split('.');
         const fileInfo = `${fileName} - ${fileExtension} - ${stats.size} bytes\n`;
         stdout.write(fileInfo);
