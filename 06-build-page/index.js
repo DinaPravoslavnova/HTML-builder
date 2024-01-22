2f073101b1ab8d40476c69b7fa7ef963ee7ec4ef
Dina P <dinchik@bk.ru>
2024-01-23 01:41:15 +0400

    refactor: add merge styles, build index, copy assets into separate functions

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -27,67 +27,79 @@ const options = {
   },
 };
 
-fs.rm(distPath, options.rm, (_error) => {
-  fs.mkdir(distPath, options.mkdir, (_error) => {
-    fs.readdir(assetsPath, options.readdir, (_error, folders) => {
-      folders.forEach((folder) => {
-        fs.mkdir(
-          path.join(distAssetsPath, folder.name),
-          options.mkdir,
-          (_error) => {
-            fs.readdir(
-              path.join(assetsPath, folder.name),
-              options.readdir,
-              (_error, files) => {
-                files.forEach((file) => {
-                  fs.copyFile(
-                    path.join(assetsPath, folder.name, file.name),
-                    path.join(distAssetsPath, folder.name, file.name),
-                    (_error) => {},
-                  );
-                });
-              },
+const copyAssets = () => {
+  fs.readdir(assetsPath, options.readdir, (_error, folders) => {
+    folders.forEach((folder) => {
+      fs.mkdir(
+        path.join(distAssetsPath, folder.name),
+        options.mkdir,
+        (_error) => {
+          fs.readdir(
+            path.join(assetsPath, folder.name),
+            options.readdir,
+            (_error, files) => {
+              files.forEach((file) => {
+                fs.copyFile(
+                  path.join(assetsPath, folder.name, file.name),
+                  path.join(distAssetsPath, folder.name, file.name),
+                  (_error) => {},
+                );
+              });
+            },
+          );
+        },
+      );
+    });
+  });
+};
+
+const mergeStyles = () => {
+  fs.readdir(stylesPath, (_error, files) => {
+    const styleWriteStream = fs.createWriteStream(distStylePath);
+
+    files.forEach((file) => {
+      const isCSS = path.parse(file).ext === '.css';
+      if (isCSS) {
+        const styleReadStream = fs.createReadStream(
+          path.join(stylesPath, file),
+          options.encoding,
+        );
+
+        styleReadStream.pipe(styleWriteStream);
+      }
+    });
+  });
+};
+
+const buildIndex = () => {
+  fs.readFile(templatePath, options.encoding, (_error, data) => {
+    let indexHTML = data;
+
+    fs.readdir(componentsPath, options.readdir, (_error, components) => {
+      components.forEach((component) => {
+        fs.readFile(
+          path.join(componentsPath, component.name),
+          options.encoding,
+          (_error, componentHTML) => {
+            const componentName = component.name.split('.')[0];
+            indexHTML = indexHTML.replace(
+              `{{${componentName}}}`,
+              componentHTML,
             );
+
+            const indexWriteStream = fs.createWriteStream(distIndexPath);
+            indexWriteStream.write(indexHTML);
           },
         );
       });
     });
+  });
+};
 
-    fs.readdir(stylesPath, (_error, files) => {
-      const styleWriteStream = fs.createWriteStream(distStylePath);
-
-      files.forEach((file) => {
-        const isCSS = path.parse(file).ext === '.css';
-        if (isCSS) {
-          const styleReadStream = fs.createReadStream(
-            path.join(stylesPath, file),
-            options.encoding,
-          );
-          styleReadStream.pipe(styleWriteStream);
-        }
-      });
-    });
-
-    fs.readFile(templatePath, options.encoding, (_error, data) => {
-      let indexHTML = data;
-      fs.readdir(componentsPath, options.readdir, (_error, components) => {
-        components.forEach((component) => {
-          fs.readFile(
-            path.join(componentsPath, component.name),
-            options.encoding,
-            (_error, componentHTML) => {
-              const componentName = component.name.split('.')[0];
-              indexHTML = indexHTML.replace(
-                `{{${componentName}}}`,
-                componentHTML,
-              );
-
-              const indexWriteStream = fs.createWriteStream(distIndexPath);
-              indexWriteStream.write(indexHTML);
-            },
-          );
-        });
-      });
-    });
+fs.rm(distPath, options.rm, (_error) => {
+  fs.mkdir(distPath, options.mkdir, (_error) => {
+    copyAssets();
+    mergeStyles();
+    buildIndex();
   });
 });
